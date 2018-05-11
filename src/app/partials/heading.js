dddc3d4d8bfa548e158fc18890517460e61e53ff
Lemmon <lemmon@users.noreply.github.com>
2018-05-11 07:59:20 +0200

    headings update

--- src/app/partials/heading.js
+++ src/app/partials/heading.js
@@ -2,7 +2,9 @@ const html = require('nanohtml')
 const css = require('../utils/css')
 
 module.exports = caption => caption && html`
-  <div class=${css('p05', 'ac')}>
-    -- ${caption} --
+  <div class=${css('ac', 'row', 'items-center', 'heading', 'gray')}>
+    <div class=${css('p05', 'span1', 'div')}></div>
+    <div class=${css('p05')}>${caption}</div>
+    <div class=${css('p05', 'span1', 'div')}></div>
   </div>
 ` || ``
