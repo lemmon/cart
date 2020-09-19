2b96a7a956c0e9029b1c67eec3df93c38b0dc263
Lemmon <lemmon@users.noreply.github.com>
2020-09-19 09:57:28 +0200

    product__actions

--- src/app/render/product.js
+++ src/app/render/product.js
@@ -26,23 +26,25 @@ module.exports = (curr, cart) => html`
       <div class="cart__product__price__total">${format.number(curr.total, 2)}</div>
       ${curr.count > 1 ? html`<div class="cart__product__price__unit">${i18n.t('price.unit', format.number(curr.price, 2))}</div>` : ``}
     </div>
-    <div class="cart__product__count">
-      <button class="cart__button cart__product__count__decrease" aria-label="Decrease Count" onclick=${e => {
-        e.preventDefault()
-        cart.decreaseProductCount(curr)
-        cart.render()
-      }}><svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none">
-        <line x1="7" y1="12" x2="17" y2="12" />
-      </svg></button>
-      <div class="cart__product__count__value">${format.number(curr.count)}</div>
-      <button class="cart__button cart__product__count__increase" aria-label="Increase Count" onclick=${e => {
-        e.preventDefault()
-        cart.increaseProductCount(curr)
-        cart.render()
-      }}><svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none">
-        <line x1="7" y1="12" x2="17" y2="12" />
-        <line y1="7" x1="12" y2="17" x2="12" />
-      </svg></button>
+    <div class="cart__product__actions">
+      <div class="cart__product__count">
+        <button class="cart__button cart__product__count__decrease" aria-label="Decrease Count" onclick=${e => {
+          e.preventDefault()
+          cart.decreaseProductCount(curr)
+          cart.render()
+        }}><svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none">
+          <line x1="7" y1="12" x2="17" y2="12" />
+        </svg></button>
+        <div class="cart__product__count__value">${format.number(curr.count)}</div>
+        <button class="cart__button cart__product__count__increase" aria-label="Increase Count" onclick=${e => {
+          e.preventDefault()
+          cart.increaseProductCount(curr)
+          cart.render()
+        }}><svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none">
+          <line x1="7" y1="12" x2="17" y2="12" />
+          <line y1="7" x1="12" y2="17" x2="12" />
+        </svg></button>
+      </div>
     </div>
   </article>
 `
